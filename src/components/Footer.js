5c70d3a8c953bbd1d63baab56a58269ebebebd8c
Aʙɪsʜɪᴋᴛʜ Jᴏs <73195851+abishikth-jos@users.noreply.github.com>
2025-03-21 00:22:39 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
         <div className='flex items-center justify-center gap-4'>
-          <Link to="/" >About</Link>
-          <Link to="/">Contact</Link>
+          <Link to="https://t.me/Ts_bots" >About</Link>
+          <Link to="https://t.me/Ts_bots">Contact</Link>
         </div>
         <p className='text-sm'>Trackstudio | Copyrights 2025</p>
     </footer>
